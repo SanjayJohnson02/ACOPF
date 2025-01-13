cadff0ded7114b22765bd69a76c902921118c36a
Sanjay Johnson <sanjay.johnson@hotmail.com>
2025-01-13 10:51:55 -0500

    jan 13

--- test2.jl
+++ test2.jl
@@ -1,2 +1,18 @@
-a = 3
-a=5
+using Pkg
+using PowerModels
+
+my_data = PowerModels.parse_file("pglib_opf_case14_ieee.m")
+
+#Pkg.add(url = "https://github.com/exanauts/ExaModelsPower.jl")
+
+using ExaModelsPower
+em, emvars = ExaModelsPower.opf_model("pglib_opf_case14_ieee.m")
+
+#Pkg.add("ExaModels")
+#Pkg.add("NLPModelsIpopt")
+using ExaModels
+using NLPModelsIpopt
+result2 = ipopt(em)
+
+data, dicts = ExaModelsPower.parse_ac_power_data("pglib_opf_case14_ieee.m")
+println(data[4])
